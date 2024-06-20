784242898fab8cd4f8d0ba476faa7aa4ad014bb8
Ariunkhishigz <ariunkhishig.zuzaan@gmail.com>
2024-06-20 14:54:08 -0500

    removed menu

--- app/src/components/layouts/Sidebar.js
+++ app/src/components/layouts/Sidebar.js
@@ -41,21 +41,21 @@ const Sidebar = () => {
               </Link>
             </li>
           )}
-          <li>
-            <Link
-              to={user.auth_type === "seller" ? "/seller/review" : "/admin/review"}
-              className="flex items-center p-2 rounded-lg hover:bg-gray-100  group">
-              <svg
-                className="flex-shrink-0 w-5 h-5  transition duration-75 group-hover:text-gray-900"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20">
-                <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-              </svg>
-              <span className="ml-6">Review</span>
-            </Link>
-          </li>
+          {user.auth_type === "admin" && (
+            <li>
+              <Link to="/admin/review" className="flex items-center p-2 rounded-lg hover:bg-gray-100  group">
+                <svg
+                  className="flex-shrink-0 w-5 h-5  transition duration-75 group-hover:text-gray-900"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20">
+                  <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span className="ml-6">Review</span>
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
