b47426517424d55c2c01380c37515f347962cb49
Ariunkhishigz <ariunkhishig.zuzaan@gmail.com>
2024-06-20 15:23:08 -0500

    header bug

--- app/src/components/layouts/Header.js
+++ app/src/components/layouts/Header.js
@@ -15,8 +15,6 @@ const Header = () => {
   const [subOpen, setSubOpen] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
-  const fetchCategories = () => {};
-
   const handleOpen = () => {
     setSubOpen(true);
   };
@@ -53,34 +51,33 @@ const Header = () => {
   };
 
   return (
-    <div className='header'>
-      <div className='top'>
-        <Link to='/'>
-          <img className='h-10' alt='logo' src={logo} />
+    <div className="header">
+      <div className="top">
+        <Link to="/">
+          <img className="h-10" alt="logo" src={logo} />
         </Link>
         <div className="flex gap-2 items-between shrink-0">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="size-6">
-
             <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
             />
           </svg>
           {user ? (
-            <Link className='cursor-pointer hover:underline' to='/user/profile'>
+            <Link className="cursor-pointer hover:underline" to="/user/profile">
               {user.fullName}
             </Link>
           ) : (
-            <Link className='cursor-pointer hover:underline' to='/login'>
+            <Link className="cursor-pointer hover:underline" to="/login">
               Sign in
             </Link>
           )}
         </div>
       </div>
       {!user || user.auth_type === "buyer" ? (
-        <div className='flex justify-between h-18 items-center px-8 py-4 font-medium border-b-2 border-solid border-black'>
-          <div className='flex gap-12 items-center relative'>
+        <div className="flex justify-between h-18 items-center px-8 py-4 font-medium border-b-2 border-solid border-black">
+          <div className="flex gap-12 items-center relative">
             {categories.map((i) => (
               <div className="relative">
                 <Link className="hover:underline text-black cursor-pointer" to="">
@@ -104,26 +101,15 @@ const Header = () => {
             <div className="relative">
               <button className="border-none absolute top-2 left-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="size-6">
-          <div className='flex gap-4 items-center'>
-            <div className='relative'>
-              <button className='border-none absolute top-2 left-4'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth='1.5'
-                  stroke='black'
-                  className='size-6'
-                >
                   <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                   />
                 </svg>
               </button>
               <input
-                placeholder='Search items ...'
+                placeholder="Search items ..."
                 onChange={handleChangeInput}
                 onKeyDown={handleKeyDown}
                 className={`focus:ring-teal-500 focus:border-teal-500 focus:outline-teal-500 shadow appearance-none border rounded w-48 md:w-72 py-2 px-3 pl-12 text-gray-700 leading-tight`}
@@ -133,11 +119,8 @@ const Header = () => {
               <img src={cart} alt="cart" className="h-9 w-9" />
               <span className="absolute bg-yellow-500 rounded-full h-4 w-4 top-0 right-0 text-xs flex items-center justify-center text-white">5</span>
             </button>
-            <button
-              className='relative cursor-pointer'
-              onClick={onClickHistory}
-            >
-              <img src={list} alt='list' className='h-8 w-8' />
+            <button className="relative cursor-pointer" onClick={onClickHistory}>
+              <img src={list} alt="list" className="h-8 w-8" />
             </button>
           </div>
         </div>
