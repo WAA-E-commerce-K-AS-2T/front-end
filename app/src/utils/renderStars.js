d70c62260e40bb050eeeb542184e6e01217412ae
sereneblack <sunsetsin1.sy@gmail.com>
2024-06-20 14:42:02 -0500

    fix: showing full stars issue

--- app/src/utils/renderStars.js
+++ app/src/utils/renderStars.js
@@ -2,8 +2,9 @@ import { StarIcon } from "@heroicons/react/solid";
 
 export const renderStars = (rating, color) => {
   if (!rating || isNaN(rating)) return null;
+  console.log("lllllll", rating);
 
-  const roundedRating = Math.round(rating);
+  const roundedRating = Math.floor(rating);
   return (
     <>
       {[...Array(roundedRating)].map((_, i) => (
@@ -13,10 +14,7 @@ export const renderStars = (rating, color) => {
         />
       ))}
       {[...Array(5 - roundedRating)].map((_, i) => (
-        <StarIcon
-          key={i}
-          className={`w-4 h-4 text-${color}-500 flex-shrink-0`}
-        />
+        <StarIcon key={i} className={`w-4 h-4 text-gray-500 flex-shrink-0`} />
       ))}
     </>
   );
