a180372d735f77533ded3e918ed4bb4a86ef83a3
Ariunkhishigz <ariunkhishig.zuzaan@gmail.com>
2024-06-20 21:53:14 -0500

    filter bug

--- app/src/components/product/ProductCard.js
+++ app/src/components/product/ProductCard.js
@@ -1,30 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { renderStars } from "../../utils/renderStars";
-const ProductCard = ({
-  id,
-  image,
-  name,
-  price,
-  description,
-  reviews,
-  totalReviews,
-  averageRating,
-}) => {
+const ProductCard = ({ id, image, name, price, description, reviews, totalReviews, averageRating }) => {
   return (
     <Link to={`/product/${id}`} className="block">
       <div className="flex flex-col items-center justify-start w-full h-full p-4 border border-gray-200 rounded-lg shadow-md">
-        <img src={image[0]?.imageUrl} alt={name} className="w-20 h-20 mb-2" />
+        <img src={image && image[0]?.imageUrl} alt={name} className="w-20 h-20 mb-2" />
         <span className="text-sm font-bold text-gray-900">$ {price}</span>
         <span className="text-sm text-gray-700">{name}</span>
         <span className="text-xs text-gray-500">{description}</span>
         <span className="flex items-center mt-2">
-          <span className="inline-flex text-[#000] mr-1">
-            {renderStars(averageRating, "yellow")}
-          </span>
-          <span className="text-xs text-gray-500 ml-1 truncate">
-            ({reviews ? reviews : "No reviews to show"})
-          </span>
+          <span className="inline-flex text-[#000] mr-1">{renderStars(averageRating, "yellow")}</span>
+          <span className="text-xs text-gray-500 ml-1 truncate">({reviews ? reviews : "No reviews to show"})</span>
           <span className="text-xs text-gray-500 ml-1 truncate">
             ({totalReviews} {totalReviews === 1 ? "review" : "reviews"})
           </span>
